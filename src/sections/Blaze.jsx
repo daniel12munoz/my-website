9bfd7d791f514bdcbf860e0f365fe1e923775dd6
Daniel <daniel12munoz@gmail.com>
2026-01-13 23:14:56 -0600

    Update Blaze video 24 Cloudflare Stream URL

--- src/sections/Blaze.jsx
+++ src/sections/Blaze.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './blaze.css';
 import PlayToggleVideo from '../components/PlayToggleVideo';
+import HlsVideo from '../components/HlsVideo';
 
 const BLAZE_HERO_LINK = {
   href: 'https://youtube.com/@DanielMunozBusiness/',
@@ -37,6 +38,38 @@ const BLAZE_VIDEO_LINKS = [
   { href: 'https://youtube.com/watch?v=8iP4Lt6X5Fs&t', label: 'youtube.com/watch?v=8iP4Lt6X5Fs&t' },   // video 25
   { href: 'https://youtube.com/watch?v=HM_1McM6Wc4', label: 'youtube.com/watch?v=HM_1McM6Wc4' },       // video 26
 ];
+
+// Mapping from video index to Cloudflare Stream HLS URLs
+const BLAZE_SHORT_VIDEO_MAP = {
+  2: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/17a21beb4593118a1a9f1e330566b2f3/manifest/video.m3u8',
+  3: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/20c375e2ea549ef63fd9468429cd41f8/manifest/video.m3u8',
+  4: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/24a0279c508fee71d4cdb197b62e1f0d/manifest/video.m3u8',
+  5: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/6d87119a2c45b2a41be2df9dbb6f5121/manifest/video.m3u8',
+  6: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/5c637ad62cb31ac76c98253e35ddbca1/manifest/video.m3u8',
+  7: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/0c6f9fb0eb65eadefbfddf46f663fb0d/manifest/video.m3u8',
+  8: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/f9546d1073e291f779d6f7ee15c3eb0a/manifest/video.m3u8',
+  9: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/cfc19dc00c0f7429b7fde350b4aa6fd9/manifest/video.m3u8',
+  10: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/93e4b8bfb3eae25121881c427fdf15fa/manifest/video.m3u8',
+  11: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/68c78f265673f4f60d2797576f479d50/manifest/video.m3u8',
+  12: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/14321fd5db47a482025001ac6edd4095/manifest/video.m3u8',
+  13: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/62de9e9b046013bae59d860f667f3c34/manifest/video.m3u8',
+  14: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/91b1dfbdf9d071e1bc2319f58be1f48f/manifest/video.m3u8',
+  15: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/ff49afc130d8e14d1ba5b2dc62d48ab8/manifest/video.m3u8',
+  16: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/36f0729f83fc6fe83ef352340638e58c/manifest/video.m3u8',
+  17: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/d2cf8a55ec314abde1c02f3f8aade29d/manifest/video.m3u8',
+  18: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/11c2a0879413bd18165da6bcf0988d0a/manifest/video.m3u8',
+  19: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/0e61ecbe230f27ed47ee434e4eef29be/manifest/video.m3u8',
+  20: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/98cb9420ebea59a3cb20d6cf2dc3f1be/manifest/video.m3u8',
+  21: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/a5ce9cef1cd57ab1aa23fc27f5000db9/manifest/video.m3u8',
+};
+
+const BLAZE_LONG_VIDEO_MAP = {
+  22: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/bc47e6f78f3b2e713876c97f6f285531/manifest/video.m3u8',
+  23: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/54d225bb8eca0e7fecfccb78ee1e9696/manifest/video.m3u8',
+  24: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/4d61a1628a9f4640332679cf4ba75fbd/manifest/video.m3u8',
+  25: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/ae847f443f7c483e61308656d681bf50/manifest/video.m3u8',
+  26: 'https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/e22257108d2eef2c0e70c44e6fdb1aa0/manifest/video.m3u8',
+};
 
 const BlazeShort = ({ src, href, label, clipId, activeBlazeClipId, setActiveBlazeClipId }) => (
   <div className="bl__shortCard">
@@ -97,9 +130,9 @@ export default function Blaze() {
       <div className="bl__row1">
         <div className="bl__videoBlock">
           <div className="media media--video bl__heroMedia">
-            <video
+            <HlsVideo
               className="bl__heroVideo"
-              src="/pre-blvideo1c.mp4"
+              src="https://customer-j47qk7l1wwcd8bxv.cloudflarestream.com/eb7c470c81bbd956afe1e07e21f77d8e/manifest/video.m3u8"
               autoPlay
               muted
               loop
@@ -159,7 +192,7 @@ export default function Blaze() {
             return (
               <BlazeShort
                 key={index}
-                src={`/bl-short${index}.mp4`}
+                src={BLAZE_SHORT_VIDEO_MAP[index]}
                 href={link.href}
                 label={link.label}
                 clipId={`blaze-short-${index}`}
@@ -184,7 +217,7 @@ export default function Blaze() {
             return (
               <BlazeVideo
                 key={index}
-                src={`/bl-video${index}.mp4`}
+                src={BLAZE_LONG_VIDEO_MAP[index]}
                 href={link.href}
                 label={link.label}
                 clipId={`blaze-long-${index}`}
