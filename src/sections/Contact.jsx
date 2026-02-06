9f713bfba6b7b7a3fcd6bd68d515fbbb08143156
Daniel <daniel12munoz@gmail.com>
2026-02-06 05:23:24 -0600

    Fixed Instagram link

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -11,10 +11,10 @@ export default function Contact() {
 
         <a
           className="contact__ig"
-          href="https://instagram.com/_vonix"
+          href="https://www.instagram.com/vonix.jpg"
           target="_blank"
           rel="noreferrer"
-          aria-label="Instagram: _vonix"
+          aria-label="Instagram: vonix.jpg"
         >
           {/* Inline SVG so we don't depend on PNG files */}
           <svg
@@ -29,7 +29,7 @@ export default function Contact() {
               d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9A5.5 5.5 0 0 1 16.5 22h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2zm0 2A3.5 3.5 0 0 0 4 7.5v9A3.5 3.5 0 0 0 7.5 20h9A3.5 3.5 0 0 0 20 16.5v-9A3.5 3.5 0 0 0 16.5 4h-9zm10.25 1.75a.9.9 0 1 1 0 1.8.9.9 0 0 1 0-1.8zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6z"
             />
           </svg>
-          <span className="contact__igHandle">_vonix</span>
+          <span className="contact__igHandle">vonix.jpg</span>
         </a>
       </div>
     </section>
